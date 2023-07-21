914569554762baaa5313e4346dbb0f61779e9129
Mahdi Khalili Hezarjaribi <109514287+Proman9473@users.noreply.github.com>
2023-07-21 17:05:09 +0200

    Create xmlspec.py

---
+++ xmlspec.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+
+# Load the image and its label in xml format
+img = plt.imread('2.jpg')
+tree = ET.parse('2.xml')
+root = tree.getroot()
+
+# Plot the image
+fig, ax = plt.subplots()
+ax.imshow(img)
+
+# Specify the object number to plot
+desired_object_number = '10'
+
+# Loop through the objects and plot them on the image
+for obj in root.iter('object'):
+    label = obj.find('name').text
+    if label == desired_object_number:
+        bndbox = obj.find('bndbox')
+        xmin = int(round(float(bndbox.find('xmin').text)))
+        ymin = int(round(float(bndbox.find('ymin').text)))
+        xmax = int(round(float(bndbox.find('xmax').text)))
+        ymax = int(round(float(bndbox.find('ymax').text)))
+        rect = plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, fill=False, edgecolor='g', linewidth=2)
+        ax.add_patch(rect)
+        ax.text(xmin, ymin-5, label, color='g', fontsize=10)
+
+
+
+# Show the image with the added bounding box
+plt.show()
